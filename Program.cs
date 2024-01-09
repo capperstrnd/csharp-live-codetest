34fc71f25205f4097e81941f8dbc60021396e0f0
Casper Strand <casperagstrand@gmail.com>
2024-01-09 14:30:39 +0100

    Uppgift 3

--- Program.cs
+++ Program.cs
@@ -66,23 +66,37 @@
     {
         Console.WriteLine("Hello");
 
-        var filtered = GetEmployeesBasedOnAge(25);
+#region previous
 
-        foreach(var employee in filtered) {
-            Console.WriteLine(employee.Name);
-        }
+        // var filtered = GetEmployeesBasedOnAge(25);
 
-        var namesAndSkills = GetEmployeeAndSkills();
+        // foreach (var employee in filtered)
+        // {
+        //     Console.WriteLine(employee.Name);
+        // }
 
-        foreach(var employee in namesAndSkills) {
-            Console.WriteLine(employee.Key);
-            Console.WriteLine("Skills: ");
-            foreach (var skill in employee.Value) {
-                Console.Write(skill + ", ");
-            }
-        }
+        // var namesAndSkills = GetEmployeeAndSkills();
+
+        // foreach (var employee in namesAndSkills)
+        // {
+        //     Console.WriteLine(employee.Key);
+        //     Console.WriteLine("Skills: ");
+        //     foreach (var skill in employee.Value)
+        //     {
+        //         Console.Write(skill + ", ");
+        //     }
+        // }
+
+#endregion
+
+        var weekday = Weekday.Monday;
+
+        string greeting = await GetDailyGreeting(weekday);
+
+        Console.WriteLine(greeting);
     }
-    public static List<Employee> GetEmployeesBasedOnAge(int ageLimit) {
+    public static List<Employee> GetEmployeesBasedOnAge(int ageLimit)
+    {
         var filteredList = new List<Employee>();
 
         filteredList = Employees.Where(x => x.Age > ageLimit).ToList();
@@ -90,13 +104,16 @@
         return filteredList;
     }
 
-    public static Dictionary<string, string[]> GetEmployeeAndSkills() {
+    public static Dictionary<string, string[]> GetEmployeeAndSkills()
+    {
+
         var employees = new Dictionary<string, string[]>();
-        
+
         var filteredList = Employees.Where(x => x.Name.StartsWith(startingLetter)).ToList();
 
-        foreach(var employee in filteredList) {
-            
+        foreach (var employee in filteredList)
+        {
+
             var name = employee.Name;
 
             if (employees.ContainsKey(name))
@@ -111,9 +128,30 @@
 
         return employees;
     }
+
+    public static async Task<string> GetDailyGreeting(Weekday day)
+    {
+        switch (day)
+        {
+            case Weekday.Monday:
+                return "Happy monday!";
+            case Weekday.Tuesday:
+                return string.Empty;
+            case Weekday.Wednesday:
+                return string.Empty;
+            case Weekday.Thursday:
+                return string.Empty;
+            case Weekday.Friday:
+                return "Relax";
+            case Weekday.Saturday:
+                return "Relax";
+            case Weekday.Sunday:
+                return "Relax";
+        }
+
+        return "";
+    }
 }
-
-
 
 public class Employee
 {
@@ -126,9 +164,7 @@ public class Employee
 }
 
 public enum Weekday
-
 {
-
     Monday,
 
     Tuesday,
